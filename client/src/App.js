7e89a9f96b1128b10358f6c9f163f83fb98ba484
Adam Horvath <adamhorvath108@gmail.com>
2021-05-04 16:41:28 -0700

    client side finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Router, navigate } from '@reach/router';
+
+import Navigation from './components/Navigation';
+import Login from './components/Login';
+import Protected from './components/Protected';
+import Content from './components/Content';
+import Register from './components/Register';
+
+export const UserContext = React.createContext([]);
 
 function App() {
+  const [ user, setUser ] = useState({});
+  const [ loading, setLoading ] = useState(true);
+
+  const logOutCallback = async () => {
+    await fetch('http://localhost:4000/logout', {
+      method: 'POST',
+      credentials: 'include'
+    })
+    //Clear user from context
+    setUser({});
+    navigate('/');
+  }
+  // if refresh_token exists, get a new access token
+  useEffect(()=> {
+    async function checkRefreshToken() {
+      const result = await(await fetch('http://localhost:4000/refresh_token', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application-json'
+        }
+      })).json();
+      setUser({
+        accesstoken: result.accesstoken
+      });
+      setLoading(false);
+    }
+    //can't have an async useEffect, so we need to create an async function inside and call it
+    checkRefreshToken();
+  }, [])
+
+  if(loading) return <div>Loading ...</div>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[user, setUser]}>
+       <div className="app">
+         <Navigation logOutCallback={logOutCallback}/>
+         <Router id="router">
+           <Login path="login" />
+           <Register path="register" />
+           <Protected path="protected" />
+           <Content path="/" />
+         </Router>
+       </div>
+    </UserContext.Provider>
+
   );
 }
 
---
+++ client/src/components/Protected.js
@@ -0,0 +1,27 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { UserContext } from '../App'
+
+const Protected = () => {
+  const [user] = useContext(UserContext);
+  const [content, setContent] = useState('You need to login');
+
+  useEffect(()=>{
+    async function fetchProtected() {
+      const result = await(await fetch('http://localhost:4000/protected', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application-json',
+          authorization: `Bearer ${user.accesstoken}`
+        }
+      })).json();
+      if(result.data) setContent(result.data);
+      console.log(result)
+    }
+
+    fetchProtected();
+  }, [user])
+
+  return <div>{content}</div>
+}
+
+export default Protected;
